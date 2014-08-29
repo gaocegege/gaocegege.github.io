9b91f2228fef3bdcf1d7ca6037e109652b0834c1
Gao Ce <gaocegege@hotmail.com>
2014-08-29 10:44:47 +0800

    baidu analytics

---
+++ baidu.js
@@ -0,0 +1,9 @@
+var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3Fe89eeec9d543f238bf73cfae5f59042c' type='text/javascript'%3E%3C/script%3E"));
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "//hm.baidu.com/hm.js?e89eeec9d543f238bf73cfae5f59042c";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
